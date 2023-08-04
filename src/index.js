8fc8674660ec248450744347e92029e31a8d0f7e
Lorenz0velez <lorenzoveleznavarro@gmail.com>
2023-08-04 18:16:14 -0300

    cambie ClientID y Domain de auth0 por los mios

--- src/index.js
+++ src/index.js
@@ -12,8 +12,10 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
-const domain = 'dev-vcimk1mq442q83wq.us.auth0.com';
-const clientId = 'Jb0tkNGAsOvygZViD8G1VGhcM5T8FRe3';
+// const domain = 'dev-vcimk1mq442q83wq.us.auth0.com'; -->> original config
+const domain = 'dev-i11mawm4dj14cqbt.us.auth0.com';
+// const clientId = 'Jb0tkNGAsOvygZViD8G1VGhcM5T8FRe3'; -->> original config
+const clientId= 'gEpbcOoX2vEm7voZicIlZg61SOH29UI6';
 
 
 const root = createRoot(document.getElementById("root"));
